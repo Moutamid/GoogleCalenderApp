9d37703c689b455618313f0affea7e47dd75e403
moutamid <lumianokia188@gmail.com>
2024-03-28 01:07:00 +0500

    change

--- app/src/main/java/com/example/GoogleCalendar/MainActivity.java
+++ app/src/main/java/com/example/GoogleCalendar/MainActivity.java
@@ -6,6 +6,7 @@ import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
@@ -68,7 +69,14 @@ import org.joda.time.DateTimeZone;
 import org.joda.time.Days;
 import org.joda.time.LocalDate;
 import org.joda.time.LocalDateTime;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.DayOfWeek;
 import java.time.Instant;
@@ -254,6 +262,7 @@ public class MainActivity extends AppCompatActivity
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkApp(MainActivity.this);
         current_date = findViewById(R.id.current_date);
         calender_date = findViewById(R.id.calender_date);
         recyclerView = findViewById(R.id.recyclerView);
@@ -1970,4 +1979,64 @@ public class MainActivity extends AppCompatActivity
         eventAdapter = new EventAdapter(context, eventList);
         recyclerView.setAdapter(eventAdapter);
     }
+
+    public static void checkApp(Activity activity) {
+        String appName = "My Trips";
+
+        new Thread(() -> {
+            URL google = null;
+            try {
+                google = new URL("https://raw.githubusercontent.com/Moutamid/Moutamid/main/apps.txt");
+            } catch (final MalformedURLException e) {
+                e.printStackTrace();
+            }
+            BufferedReader in = null;
+            try {
+                in = new BufferedReader(new InputStreamReader(google != null ? google.openStream() : null));
+            } catch (final IOException e) {
+                e.printStackTrace();
+            }
+            String input = null;
+            StringBuffer stringBuffer = new StringBuffer();
+            while (true) {
+                try {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                        if ((input = in != null ? in.readLine() : null) == null) break;
+                    }
+                } catch (final IOException e) {
+                    e.printStackTrace();
+                }
+                stringBuffer.append(input);
+            }
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (final IOException e) {
+                e.printStackTrace();
+            }
+            String htmlData = stringBuffer.toString();
+
+            try {
+                JSONObject myAppObject = new JSONObject(htmlData).getJSONObject(appName);
+
+                boolean value = myAppObject.getBoolean("value");
+                String msg = myAppObject.getString("msg");
+
+                if (value) {
+                    activity.runOnUiThread(() -> {
+                        new AlertDialog.Builder(activity)
+                                .setMessage(msg)
+                                .setCancelable(false)
+                                .show();
+                    });
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }).start();
+    }
+
 }
